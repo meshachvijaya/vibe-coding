ecd62ca25a47b3f1e5c4c9297216a15b3ed2350b
unknown <meshachvijaya@gmail.com>
2026-04-03 12:17:39 +0700

    feat: implement user registration with bcrypt and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersController } from './routes/users.routes';
+import { UsersService } from './services/users.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
